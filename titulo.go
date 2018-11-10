3fb2c608522c7ed42881702d595a790a6fe34ef6
Antonio Henrique de Melo Costa <ahmcosta@gmail.com>
2018-11-10 12:05:54 -0200

    Initial commit

---
+++ titulo.go
@@ -0,0 +1,28 @@
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"time"
+)
+
+/*
+	This function expects a URLs array. For each URL, it gets the page, cuts its body, search its title using regex
+*/
+func titulo(urls ...string) <-chan string { // <-chan - canal somente-leitura
+	c := make(chan string)
+	for _, url := range urls {
+		go func(url string) { // anonymous function
+			beginTime := time.Now()
+			resp, _ := http.Get(url)             // get the page by url
+			html, _ := ioutil.ReadAll(resp.Body) // get the body of the page gotten
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>") // compile the regex
+			title := r.FindStringSubmatch(string(html))[1]
+			finishTime := time.Now()
+			totalTime := finishTime.Sub(beginTime)
+			c <- fmt.Sprintf("Elapsed Time: %d\tTitle: %s", totalTime, title) // return the string from compiled regex
+		}(url) // It's needed because it's a anonymous functions. So it must be called
+	}
+	return c
+}
